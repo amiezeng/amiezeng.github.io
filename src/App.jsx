70022333e6e864341eff1ee5e0a8ef3196c35b94
Amie Zeng <amiezeng@mac.mynetworksettings.com>
2026-05-20 15:57:47 -0400

    small adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -191,7 +191,7 @@ export default function App() {
   };
 
   return (
-    <div style={{ width: "100vw", height: "100vh", overflow: "hidden", position: "relative", cursor: "crosshair" }}>
+    <div style={{ width: "100%", height: "100%", overflow: "hidden", position: "fixed", top: 0, left: 0, cursor: "crosshair" }}>
       <div style={{
         position: "absolute", inset: 0,
         background: "url('/background1.jpg') center/cover no-repeat",
